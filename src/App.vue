6691f79d2beddcf894d5fd7ebbcebf0676d92aa7
siriyusabbos <siriyusabbos@gmail.com>
2023-02-07 20:53:14 +0500

    ыпквкап
    пенане

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,22 @@
 <script setup>
 import HelloWorld from './components/HelloWorld.vue'
+import HomeFooter from './components/HomeFooter.vue';
 import TheWelcome from './components/TheWelcome.vue'
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
   <main>
-    <TheWelcome />
+    <HomeFooter/>
   </main>
 </template>
-
+<style>
+*{
+  padding:0px;
+margin:0px}
+body {
+  background-color: black;
+}
+</style>
 <style scoped>
 header {
   line-height: 1.5;
---
+++ src/components/HomeFooter.vue
@@ -0,0 +1,706 @@
+<template>
+  <div class="all">
+<div class="main-2">
+    <img src="./aimg/free-icon-quote-565703 1 (Traced).png" alt="">
+    <p>Цены на наши услуги всегда доступны и полностью соотносятся как с текущими рыночными
+        тенденциями, так и с прейскурантами, размещенными в соответствующих разделах сайта. Обращаясь к нам
+        за помощью, вы получаете недорогой профессиональный ремонт, корректное обслуживание, адекватные цены и неизменно
+        положительные результаты затраченных усилий мастера</p>
+</div>
+<div class="divhr"></div>
+<div class="main-4">
+    <h1>Вызовите специалиста на дом<br>
+        <span>Приедем в любой район</span> Санкт-Петербурга
+    </h1>
+    <span>Мы приедем, починим Ваше устройство или заберем
+        его в сервисный центр для дальнейшего ремонта</span>
+
+    <img class="er" src="./aimg/Vector 1.png" alt="">
+    <div class="acards3">
+        <div id="ere" class="acards31">
+            <div class="acard3">
+                <img src="./aimg/Metro_Sankt-Petersburg 9.png" alt="">
+                <h5>Красная ветка</h5>
+                <p>Все станции красной ветки метрополитена
+                    и районы вокруг</p>
+            </div>
+            <div class="acard3"> <img src="./aimg/Metro_Sankt-Petersburg 9.png" alt="">
+                <h5>Красная ветка</h5>
+                <p>Все станции красной ветки метрополитена
+                    и районы вокруг</p>
+            </div>
+            <div class="acard3"> <img src="./aimg/Metro_Sankt-Petersburg 9.png" alt="">
+                <h5>Красная ветка</h5>
+                <p>Все станции красной ветки метрополитена
+                    и районы вокруг</p>
+            </div>
+        </div>
+        <div class="acards31">
+            <div class="acard3"> <img src="./aimg/Metro_Sankt-Petersburg 9.png" alt="">
+                <h5>Красная ветка</h5>
+                <p>Все станции красной ветки метрополитена
+                    и районы вокруг</p>
+            </div>
+            <div class="acard3"> <img src="./aimg/Metro_Sankt-Petersburg 9.png" alt="">
+                <h5>Красная ветка</h5>
+                <p>Все станции красной ветки метрополитена
+                    и районы вокруг</p>
+            </div>
+            <div class="acard3"> <img src="./aimg/Metro_Sankt-Petersburg 9.png" alt="">
+                <h5>Красная ветка</h5>
+                <p>Все станции красной ветки метрополитена
+                    и районы вокруг</p>
+            </div>
+        </div>
+    </div>
+</div>
+<div class="divhr"></div>
+
+
+<div class="main-3">
+    <h1><span>Принимаем к оплате</span><br>
+        наличные, карты и переводы</h1>
+    <div class="acards2">
+        <div class="acard2">
+            <div>
+                <h6>Наличный рассчет</h6>
+                <p>Оплата услуг наличными</p>
+            </div>
+            <img src="./aimg/money-dynamic-color.png" alt="">
+        </div>
+        <div class="acard2">
+            <div>
+                <h6>Банковские карты</h6>
+                <p>Оплата через терминал</p>
+            </div>
+            <img src="./aimg/money-dynamic-color.png" alt="">
+        </div>
+        <div class="acard2">
+            <div>
+                <h6>Безналичный рассчет</h6>
+                <p>Безналичная оплата услуг</p>
+            </div>
+            <img src="./aimg/money-dynamic-color.png" alt="">
+        </div>
+    </div>
+    <div class="acards3"><img src="./aimg/image 86 (Traced).png" alt=""><img src="./aimg/image 86 (Traced).png"
+            alt=""><img src="./aimg/image 86 (Traced).png" alt=""></div>
+</div>
+
+
+<div class="main-2">
+    <img src="./aimg/free-icon-quote-565703 1 (Traced).png" alt="">
+    <p>Цены на наши услуги всегда доступны и полностью соотносятся как с текущими рыночными
+        тенденциями, так и с прейскурантами, размещенными в соответствующих разделах сайта. Обращаясь к нам
+        за помощью, вы получаете недорогой профессиональный ремонт, корректное обслуживание, адекватные цены и неизменно
+        положительные результаты затраченных усилий мастера</p>
+</div>
+<div class="divhr" style="margin-bottom:100px"></div>
+
+<main class="main-1">
+    <h1>
+        Наши <span> контактные данные</span><br>
+        для быстрой связи
+    </h1>
+    <div class="main-1k">
+        <div class="main-1b">
+            <h6>Все районы Санкт-Петербурга</h6>
+            <h2>+7 (812) 333-70-75</h2>
+            <div class="acards">
+                <div class="acard">
+                    <p>Выезд матеров</p>
+                    <h4>с 10:00 до 21:00</h4>
+                </div>
+                <div class="acard">
+                    <p>Ремонтная зона</p>
+                    <h4>Ленинский пр, 136</h4>
+                </div>
+            </div>
+            <span>Вы всегда можете связаться с нами <br>
+                через мессенджеры!</span>
+            <div class="acontacts">
+                <div class="acontact">
+                    <div class="aimg"><img style="margin:auto" src="./aimg/path2991.png" alt=""></div>
+                    <div>Telegram</div>
+                </div>
+                <div class="acontact">
+                    <img src="./aimg/Vector.png" alt="">
+                    <div>WhatsApp</div>
+                </div>
+                <div class="acontact">
+                    <div class="aimg1"><img class="aimg2" src="./aimg/Vector(1).png" alt=""><img
+                            src="./aimg/Vector(1).png" alt=""></div>
+                    <div style="margin-left:10px;">3771404@gmail.com</div>
+                </div>
+            </div>
+            <div class="divhr"></div>
+            <span>Оставьте заявку на сайте и на <br>
+                Ваш адрес приедет наш специалист</span>
+            <button>Оставить заявку на сайте</button>
+        </div>
+        <div class="main-1m">
+            <div id="map" style="width: 100%; height: 100%"></div>
+        </div>
+
+    </div>
+
+</main>
+<footer>
+    <div class="footer-logo">
+        <div class="flogo"><img src="./aimg/laptop-with-arrow 1.png" alt=""></div>
+        <h6>Компьютерный сервис</h6>
+    </div>
+    <ul>
+        <a href="">
+            <li>Главная</li>
+        </a>
+        <a href="">
+            <li>Устройства</li>
+        </a>
+        <a href="">
+            <li>Бренды</li>
+        </a>
+        <a href="">
+            <li>Районы обслуживания</li>
+        </a>
+        <a href="">
+            <li>Оставить заявку</li>
+        </a>
+        <a href="">
+            <li>О компании </li>
+        </a>
+        <a href="">
+            <li>Контакты</li>
+        </a>
+    </ul>
+    <p>©2023 Сервисный центр электроники webabbas9@gmail.com</p>
+</footer>
+
+  </div>
+</template>
+
+<script>
+</script>
+
+<style scoped>
+*{
+    padding: 0px;
+    margin: 0px;
+    font-family: 'Mulish', sans-serif;
+}
+
+
+.main-4 {
+    position: relative;
+}
+
+.main-4 h1 {
+    text-align: center;
+    font-family: 'Mulish', sans-serif;
+    font-style: normal;
+    font-weight: 800;
+    font-size: 32px;
+    line-height: 130%;
+    margin-left: 10%;
+    margin-bottom: 30px;
+    margin-top: 30px;
+    color: #FFFFFF;
+}
+
+.er {
+    position: absolute;
+    top: 30%;
+    width: 100%;
+    left: 0px;
+}
+
+.main-4 h1 span {
+
+    border-radius: 5px;
+    padding-left: 5px;
+    padding-right: 5px;
+}
+
+.acards3 {
+    display: flex;
+    justify-content: center;
+    gap: 30px;
+    /* flex-wrap:wrap; */
+    width: 100vw;
+
+}
+
+#ere {
+    position: relative;
+    top: -100px
+}
+
+@media(max-width:650px) {
+    #ere {
+        position: relative;
+        top: 0px
+    }
+
+    .acards3 {
+        display: flex;
+        flex-wrap: wrap;
+        justify-content: center;
+        gap: 30px;
+        /* flex-wrap:wrap; */
+        width: 100vw;
+
+    }
+}
+
+.acard3 {
+    flex-direction: column;
+    align-items: flex-start;
+    padding: 30px;
+    margin-bottom: 20px;
+    width: 300px;
+    /* height: 211px; */
+    /* Белый */
+    background: #FFFFFF;
+    border-radius: 30px;
+}
+
+.acard3 h5 {
+    /* Десктоп/h3 New 24px */
+    font-family: 'Gilroy';
+    font-style: normal;
+    font-weight: 800;
+    font-size: 24px;
+    line-height: 29px;
+
+    /* Черный */
+    color: #000000;
+
+
+    /* Inside auto layout */
+    flex: none;
+    order: 1;
+    flex-grow: 0;
+}
+
+.acards31 {
+    position: relative;
+}
+
+
+
+
+
+
+/* .main-3 */
+.main-3 h1 {
+    text-align: center;
+    font-family: 'Mulish', sans-serif;
+    font-style: normal;
+    font-weight: 800;
+    font-size: 32px;
+    line-height: 130%;
+    margin-left: 10%;
+    margin-bottom: 30px;
+    margin-top: 30px;
+    color: #FFFFFF;
+}
+
+.main-3 h1 span {
+    background-color: #4047FF;
+    border-radius: 5px;
+    padding-left: 5px;
+    padding-right: 5px;
+}
+
+.acards2 {
+    display: flex;
+    justify-content: space-around;
+    flex-wrap: wrap;
+
+}
+
+.acard2 {
+    width: 27%;
+    display: flex;
+    min-width: 300px;
+    justify-content: space-around;
+    align-items: center;
+    padding: 20px;
+    isolation: isolate;
+    background: #FFFFFF;
+    border-radius: 30px;
+    margin-bottom: 20px;
+}
+
+.card2 h6 {
+    font-family: 'Gilroy';
+    font-style: normal;
+    font-weight: 800;
+    font-size: 24px;
+    line-height: 29px;
+    color: #000000;
+}
+
+.card2 p {
+    font-family: 'Raleway';
+    font-style: normal;
+    font-weight: 500;
+    font-size: 15px;
+    line-height: 140%;
+    color: #949494;
+}
+
+.acards3 {
+    display: flex;
+    justify-content: space-around;
+    min-width: 300px;
+    width: 60%;
+    max-width: 450px;
+    margin: auto;
+    margin-top: 70px;
+}
+
+
+
+/* .main-2 */
+.main-2 {
+    display: flex;
+    justify-content: center;
+    align-items: flex-start;
+    margin-top: 150px;
+    margin-bottom: 100px;
+
+}
+
+.main-2 p {
+    width: 80%;
+    font-family: 'Raleway';
+    font-style: normal;
+    font-weight: 500;
+    font-size: 22px;
+    line-height: 160%;
+    text-align: justify;
+    /* or 35px */
+    font-feature-settings: 'pnum' on, 'lnum' on;
+
+    /* Белый */
+    color: #FFFFFF;
+}
+
+.main-2 img {
+    width: 5%;
+    min-width: 15px;
+    max-width: 40px;
+    margin-right: 20px;
+}
+
+@media(max-width:991px) {
+    .main-2 p {
+        font-size: 15px;
+    }
+}
+
+
+
+/* main-1 */
+.main-1 {
+    margin-bottom: 40px;
+}
+
+.main-1 h1 {
+
+    font-family: 'Mulish', sans-serif;
+    font-style: normal;
+    font-weight: 800;
+    font-size: 32px;
+    line-height: 130%;
+    margin-left: 10%;
+    margin-bottom: 30px;
+    margin-top: 30px;
+    color: #FFFFFF;
+}
+
+.main-1 h1 span {
+    background-color: #4047FF;
+    border-radius: 5px;
+    padding-left: 5px;
+    padding-right: 5px;
+}
+
+.main-1k {
+    display: flex;
+    justify-content: space-around;
+    flex-wrap: wrap;
+    /* align-items: center; */
+}
+
+.main-1b {
+
+    width: 40%;
+    min-width: 300px;
+    display: flex;
+    flex-direction: column;
+    align-items: flex-start;
+    background: #FFFFFF;
+    border-radius: 30px;
+    margin-bottom: 30px;
+}
+
+.main-1m {
+    /* background-color: red; */
+    border-radius: 30px;
+    overflow: hidden;
+    width: 40%;
+    min-height: 200px;
+    min-width: 300px;
+    margin-bottom: 30px;
+}
+
+.main-1b h6 {
+    padding-top: 30px;
+    padding-left: 10%;
+    font-style: normal;
+    font-weight: 800;
+    font-size: 24px;
+    line-height: 29px;
+
+    /* Белый */
+    color: black;
+}
+
+.main-1b span {
+    font-family: 'Raleway';
+    font-style: normal;
+    font-weight: 500;
+    font-size: 15px;
+    line-height: 140%;
+    color: #000000;
+    margin-left: 10%;
+    margin-top: 20px;
+}
+
+.main-1b h2 {
+    font-style: normal;
+    padding-top: 20px;
+    font-weight: 800;
+    font-size: 44px;
+    line-height: 110%;
+    padding-left: 10%;
+    color: black;
+}
+
+.acards {
+    margin-top: 20px;
+    display: flex;
+    justify-content: space-between;
+    width: 80%;
+    max-width: 500px;
+    margin-left: 10%;
+}
+
+.acard p {
+    font-style: normal;
+    font-weight: 600;
+    font-size: 15px;
+    line-height: 18px;
+    color: black;
+}
+
+.acard h4 {
+    font-style: normal;
+    font-weight: 800;
+    font-size: 24px;
+    line-height: 29px;
+    color: black;
+}
+
+.aimg2 {
+    position: absolute;
+    top: 5px;
+    left: 4px;
+    background-color: #ECECEC;
+}
+
+.aimg1 {
+    position: relative;
+    width: 24px;
+    height: 24px;
+}
+
+.acontacts {
+    display: flex;
+    /* justify-content: space-around; */
+    flex-wrap: wrap;
+    margin-left: 10%;
+    /* width: 100%; */
+    margin-top: 20px;
+
+}
+
+.aimg {
+    background: linear-gradient(203.2deg, #37AEE2 21.67%, #1E96C8 70%);
+    width: 24px;
+    height: 24px;
+    border-radius: 50%;
+    display: flex;
+    margin: auto;
+    align-items: center;
+    justify-content: center;
+    margin-right: 10px;
+}
+
+.aimg img {
+    margin: auto;
+}
+
+.acontact {
+    margin-right: 10px;
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+    align-items: center;
+    padding: 8px 12px;
+    background: #ECECEC;
+    border-radius: 20px;
+    margin-bottom: 10px;
+}
+
+.divhr {
+    width: 80%;
+    border: 1px solid rgb(22, 22, 22);
+    margin: auto;
+    margin-top: 20px;
+}
+
+.acontact img {
+    margin-right: 10px;
+}
+
+.main-1b button {
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+    align-items: center;
+    padding: 16px 30px;
+    background: #4047FF;
+    border: none;
+    border-radius: 20px;
+    margin-left: 10%;
+    margin-top: 20px;
+    margin-bottom: 20px;
+    font-style: normal;
+    font-weight: 800;
+    font-size: 15px;
+    line-height: 18px;
+
+
+    color: #FFFFFF;
+}
+
+@media(max-width:991px) {
+    .acard p {
+        font-size: 8px;
+    }
+
+    .main-1b h2 {
+        font-size: 24px;
+    }
+
+    .main-1b span {
+        font-size: 8px;
+    }
+
+    .acard h4 {
+        font-size: 13px;
+    }
+
+    .main-1b h6 {
+        font-size: 13px;
+    }
+}
+
+
+@media (max-width:600px) {
+    .main-1 h1 {
+        font-size: 24px;
+    }
+}
+
+/* footer */
+footer {
+    background-color: white;
+    border-top-left-radius: 30px;
+    border-top-right-radius: 30px;
+}
+
+footer ul li:hover {
+    color: #4047FF;
+    text-align: center;
+    font-family: 'Mulish', sans-serif;
+}
+
+footer h6 {
+    font-family: 'Mulish', sans-serif;
+    font-style: normal;
+    font-weight: 800;
+    font-size: 18px;
+    line-height: 22px;
+    color: #000000;
+}
+
+footer ul li {
+    transition: 1s;
+    font-family: 'Mulish', sans-serif;
+    color: #000000;
+    min-width: 200px;
+    text-align: center;
+}
+
+footer ul {
+    margin-top: 30px;
+    margin-bottom: 30px;
+    list-style: none;
+    display: flex;
+    justify-content: space-around;
+    width: 100%;
+    flex-wrap: wrap;
+
+}
+
+footer a {
+    text-decoration: none;
+}
+
+footer p {
+    text-align: center;
+    font-family: 'Raleway';
+    font-style: normal;
+    font-weight: 500;
+    font-size: 15px;
+    line-height: 140%;
+    color: #949494;
+}
+
+footer .footer-logo {
+    width: 280px;
+    display: flex;
+    align-items: center;
+    justify-content: space-around;
+    margin: auto;
+}
+
+footer .flogo {
+    width: 50px;
+    height: 50px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    background: #4047FF;
+    border-radius: 20px;
+}
+
+footer {
+    padding-top: 50px;
+    padding-bottom: 50px;
+}
+</style>
