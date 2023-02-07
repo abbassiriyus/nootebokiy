78985a4d6b9a4fe597b01f70373e43783d7dee77
ozdbek <109587664+Flander404@users.noreply.github.com>
2023-02-07 20:59:25 +0500

    yrt
    ttyr

---
+++ src/components/Main.vue
@@ -0,0 +1,40 @@
+<template>
+    <header class="header">
+        <nav class="nav">
+            <div class="nav-logo">
+                <button><i class='bx bx-laptop box-lap'></i>Компьютерный сервис</button>
+            </div>
+            <div class="nav-menu">
+                <button class="addres">Все районы города | +7 (812) 409-42-65</button>
+                <button class="call">Вызвать специалиста</button>
+                <button class="menu"><i class='bx bx-dots-horizontal-rounded'></i>Меню</button>
+            </div>
+        </nav>
+        <div class="main">
+            <div class="main-p">
+                <p><span>Скидка 10%</span>  на ремонт <br>
+                компьютерной техники при <br>
+                заявке через сайт</p>
+            </div>
+            <div class="main-none">
+                <img src="../nimg/gift-dynamic-color.png" alt="">
+            </div>
+        </div>
+        <div class="main-card">
+            <div class="room-card">
+                <div class="room-p">
+                    <div class="room-p2">
+                        
+                    </div>
+                    <h3>Ремонт на дому</h3>
+                    <p>Почините свой неисправный ноутбук <br>
+                    не выходя из квартиры</p>
+                </div>
+                <div class="room-img">
+
+                </div>
+            </div>
+        </div>
+    </header>
+</template>
+
